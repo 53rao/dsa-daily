78486674d4dc27f6dd53c30ace9a3e52e982a744
S Rao <raox58@gmail.com>
2026-07-17 13:35:40 +0530

    Time: 3 ms (40.73%), Space: 12.2 MB (97.17%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> mp;
         vector<int> soln;
         for(int i=0;i<nums1.size();i++){
             int j;
             for(j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
-                    mp[nums1[i]]=j;
                     break;
                 }
             }
