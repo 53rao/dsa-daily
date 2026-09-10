f43ca50893423d148ba5e9f2d6ffec373f4d9b05
S Rao <raox58@gmail.com>
2026-09-10 20:12:57 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.99%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+    void backtracking(vector<int>& nums,int idx,int curr,int &soln){
+        if(idx>=nums.size()){
+            soln+=curr;
+            return;
+        }
+        backtracking(nums,idx+1,curr,soln);
+        curr=curr^nums[idx];
+        backtracking(nums,idx+1,curr,soln);
+    }
     int subsetXORSum(vector<int>& nums) {
         int soln=0;
-        int n=nums.size();
-        for(int i=0;i<pow(2,n);i++){
-            int curr=0,j=i,k=n-1;
-            while(j>0){
-                if(j%2)
-                    curr=curr^nums[k];
-                k--;
-                j=j>>1;
-            }
-            soln+=curr;
-        }
+        backtracking(nums,0,0,soln);
         return soln;
     }
 };
