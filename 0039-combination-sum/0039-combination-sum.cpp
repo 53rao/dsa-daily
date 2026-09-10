01df04392987362c3de8aa65745b10c5ec12a13d
S Rao <raox58@gmail.com>
2026-09-10 20:26:30 +0530

    Time: 6 ms (26.37%), Space: 18.4 MB (18.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> soln;
-    void combination(int idx,vector<int>& candidates,vector<int> &curr,int target,int total){
-        if(total==target){
-            soln.emplace_back(curr);
+    set<vector<int>> soln;
+    void backtrack(vector<int>& candidates, int target,int curr,int idx,vector<int> &temp){
+        if(curr==target){
+            soln.insert(temp);
             return;
         }
-        if(total>target||idx==candidates.size())
+        if(idx>=candidates.size()|| curr>target)
             return;
-        curr.emplace_back(candidates[idx]);
-        combination(idx,candidates,curr,target,total+candidates[idx]);
-        curr.pop_back();
-        combination(idx+1,candidates,curr,target,total);
+        // case 1 : skip and move forward
+        backtrack(candidates,target,curr,idx+1,temp);
+        // case 2: include and  move forward
+        temp.emplace_back(candidates[idx]);
+        backtrack(candidates,target,curr+candidates[idx],idx+1,temp);
+        // case 3 : dont move forward
+        backtrack(candidates,target,curr+candidates[idx],idx,temp);
+        temp.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> curr={};
-        combination(0,candidates,curr,target,0);
-        return soln;
+        vector<int> temp;
+        backtrack(candidates,target,0,0,temp);
+        return vector<vector<int>>(soln.begin(), soln.end());
     }
 };
