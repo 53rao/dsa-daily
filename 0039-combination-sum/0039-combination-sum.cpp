e710d23a7807e8cd2a64a852cd2930d43d7f43de
S Rao <raox58@gmail.com>
2026-09-10 20:28:37 +0530

    Time: 3 ms (51.31%), Space: 18.2 MB (18.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    set<vector<int>> soln;
+    vector<vector<int>> soln;
     void backtrack(vector<int>& candidates, int target,int curr,int idx,vector<int> &temp){
         if(curr==target){
-            soln.insert(temp);
+            soln.emplace_back(temp);
             return;
         }
         if(idx>=candidates.size()|| curr>target)
             return;
         // case 1 : skip and move forward
         backtrack(candidates,target,curr,idx+1,temp);
-        // case 2: include and  move forward
+        // case 2: include and  dont move forward
         temp.emplace_back(candidates[idx]);
-        backtrack(candidates,target,curr+candidates[idx],idx+1,temp);
-        // case 3 : dont move forward
+        
         backtrack(candidates,target,curr+candidates[idx],idx,temp);
         temp.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> temp;
         backtrack(candidates,target,0,0,temp);
-        return vector<vector<int>>(soln.begin(), soln.end());
+        return soln;
     }
 };
