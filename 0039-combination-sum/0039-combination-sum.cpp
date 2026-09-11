3b25e89d1a7b1cc13203fce11fe5a63a7149c20e
S Rao <raox58@gmail.com>
2026-09-11 17:57:25 +0530

    Time: 4 ms (36.45%), Space: 14.1 MB (76.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -8,17 +8,20 @@ public:
         }
         if(idx>=candidates.size()|| curr>target)
             return;
-        // case 1 : skip and move forward
-        backtrack(candidates,target,curr,idx+1,temp,soln);
-        // case 2: include and  dont move forward
-        temp.emplace_back(candidates[idx]);
+        // case 1: include and  dont move forward
+         temp.emplace_back(candidates[idx]);
         
         backtrack(candidates,target,curr+candidates[idx],idx,temp,soln);
         temp.pop_back();
+        // case 2 : skip and move forward
+        backtrack(candidates,target,curr,idx+1,temp,soln);
+        
+       
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> temp;
         vector<vector<int>> soln;
+        sort(candidates.begin(), candidates.end());
         backtrack(candidates,target,0,0,temp,soln);
         return soln;
     }
