48c43f1c25b43f5161f0082d231c72517e9e1812
S Rao <raox58@gmail.com>
2026-09-11 18:36:36 +0530

    Time: 38 ms (69.93%), Space: 62.5 MB (71.92%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void comb(int n,int k,int index,vector<int> &curr,set<vector<int>> &soln){
-        if (curr.size() == k) {
-            soln.insert(curr);
+    
+    void backtrack(int n,int k,int size,vector<int> &temp,vector<vector<int>> &soln){
+        if(size==k){
+            soln.emplace_back(temp);
             return;
         }
-        if (index > n) {
-            return;
+        for(int i=temp.size()?temp.back()+1:1;i<=n;i++){
+            temp.emplace_back(i);
+            backtrack(n,k,size+1,temp,soln);
+            temp.pop_back();
         }
-        curr.emplace_back(index);
-        comb(n, k, index + 1, curr, soln);
-        curr.pop_back(); 
-        comb(n,k,index+1,curr,soln);
+
     }
     vector<vector<int>> combine(int n, int k) {
-        set<vector<int>> soln;
+        vector<vector<int>> soln;
         vector<int> temp;
-        comb(n,k,1,temp,soln);
-        return vector<vector<int>>{soln.begin(),soln.end()};
+        backtrack(n,k,0,temp,soln);
+        return soln;
     }
 };
